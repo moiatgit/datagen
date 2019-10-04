281066ae8167bb638f262932ddc25c2e92371679
Moisès Gómez Girón <moises@blamix230.blami.net>
2019-10-04 09:56:51 +0200

    added format scn (surname-comma-name) to generanoms

--- generanoms.py
+++ generanoms.py
@@ -9,17 +9,42 @@
 
 # Nota: aquesta versió simplement peta si no troba els fitxers
 #
-import sys, os, random
+
+import sys
+import os
+import random
+import argparse
+
+_VERSION = '1.1'
 #
 _FITXER_NOMS = "noms.dat"
 _FITXER_COGNOMS = "cognoms.dat"
 _FREQ_NOMS = 0.5       # 20% dels noms generats seran dels freqüents
 _FREQ_COGNOMS = 0.5    # 10% dels noms generats seran dels freqüents
 _FREQ_DOS_COGNOMS = 0.98    # 98% tenen dos cognoms
-#
-class GeneradorNoms:
-    def __init__(self):
 
+
+def compose_argparse():
+    """ composes and returns an ArgumentParser """
+    p = argparse.ArgumentParser(description = "Name generator")
+
+    p.add_argument('-n', action='store', default=10, type=int,
+            help=u"Number of results. Default 10.", dest='how_many')
+
+    p.add_argument('-f', '--format', action='store',
+                   choices = ['nss', 'scn'],
+                   default = 'nss',
+                   help=u"Output format. 'nss': name-space-surnames (default). 'scn': surname-coma-name")
+    p.add_argument('-v', '--version', action='version',
+                   version='%s %s' % (sys.argv[0], _VERSION))
+    return p
+
+
+class GeneradorNoms:
+    def __init__(self, output_format):
+        """ Given the output format, initializes the instance """
+
+        self.output_format = output_format
         self.noms = []
         self.cognoms = []
         self.noms_freq = []
@@ -27,17 +52,22 @@ class GeneradorNoms:
 
         self.carrega_fitxers()
 
+
     def carrega_fitxers(self):
         carrega_valors(_FITXER_NOMS, self.noms, self.noms_freq)
         carrega_valors(_FITXER_COGNOMS, self.cognoms, self.cognoms_freq)
 
     def genera_nom(self):
+        """ returns a random name from the list of names """
         llista = self.selecciona_llista_noms()
         nom = random.choice(llista)
         return nom
 
     def genera_cognom(self, accepta_frequent=True):
-        """ Es pot indicar quan no sigui acceptable un cognom freqüent."""
+        """ returns a random surname from the list of surnames
+            accepta_frequent: when False, this method won't result in a
+                              frequent surname
+        """
         if accepta_frequent:
             llista = self.selecciona_llista_cognoms()
         else:
@@ -49,21 +79,34 @@ class GeneradorNoms:
         """ genera cognoms, majoritàriament dos.
             En cas que accepta_un sigui fals, sempre en genera dos.
             En cas que accepta_frequent sigui fals, no es tindrà en
-            compte la freqüència dels cognoms """
+            compte la freqüència dels cognoms.
+
+            It returns a tuple with one or more surnames.
+            """
         if accepta_un:
             frequent = random.random() < _FREQ_DOS_COGNOMS
         else:
             frequent = True
         if frequent:
-            cognoms = "%s %s"%(self.genera_cognom(), self.genera_cognom()) 
+            cognoms = (self.genera_cognom(), self.genera_cognom())
         else:
-            cognoms = "%s"%(self.genera_cognom(False)) 
+            cognoms = (self.genera_cognom(False),)
+
         return cognoms
+
 
     def genera_nom_cognoms(self, accepta_frequent=True, accepta_un_cognom=True):
         """ composa i retorna un nom amb un o dos cognoms """
-        cognoms = self.genera_cognoms(accepta_frequent, accepta_un_cognom)
-        return "%s %s"%(self.genera_nom(), cognoms) 
+
+        name = self.genera_nom()
+        surnames = self.genera_cognoms()
+
+        if self.output_format == 'scn':
+            result = '%s, %s' % (' '.join(surnames), name)
+        else:
+            result = '%s %s'%(name, " ".join(surnames))
+        return result
+
 
     def selecciona_llista_noms(self):
         """ retorna la llista de noms generals o la freqüent, depenent
@@ -105,12 +148,13 @@ def carrega_valors(nom_fitxer, llista, llista_frequents):
 
 #
 def main():
-    try:
-        n = int(sys.argv[1])
-    except:
-        n = 10
-    generador = GeneradorNoms()
-    for i in range(n):
+    """ checks commandline args and performs accordingly """
+
+    p = compose_argparse()
+    options = p.parse_args()
+#
+    generador = GeneradorNoms(options.format)
+    for i in range(options.how_many):
         print(generador.genera_nom_cognoms())
     return 0
 #
--- todo.rst
+++ todo.rst
@@ -4,7 +4,7 @@ To Do List
 
 List of tasks to perform:
 
-* change generanoms so it accepts a format
+* (done) change generanoms so it accepts a format
 
   Now it allows just to specify the number of names
 
@@ -17,3 +17,11 @@ List of tasks to perform:
   $ generatenames.py -f name_space_surnames     # current one
   $ generatenames.py -f surname_comma_name
   $ generatenames.py -n 5                       # currently the only arg
+
+* make it more robust
+
+* consider programming it as a generator so it can be used directly from a program
+
+* add gneranoms.py commandline options to specify attributes like accepting one surname only
+
+* consider doc translation to English
